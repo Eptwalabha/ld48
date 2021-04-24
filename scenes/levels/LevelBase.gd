135a94ff56eee0795b7ac884a8ac6f383e96c8f2
damien <damiengabrielle@gmail.com>
2021-04-24 16:00:11 +0100

    tileset add dirt
    tileset add dirt

--- scenes/levels/LevelBase.gd
+++ scenes/levels/LevelBase.gd
@@ -18,3 +18,7 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 	
+	if Input.is_action_just_pressed("action"):
+		var p_position = $Player.global_position
+		var pos = $TileMap.world_to_map(p_position)
+		print("action on %s" % $TileMap.get_cellv(pos))
