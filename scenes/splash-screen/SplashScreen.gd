5673f1995ef14e74d7ee202079552bbe1990db4f
damien <damiengabrielle@gmail.com>
2021-04-26 17:45:42 +0200

    change boot splash

--- scenes/splash-screen/SplashScreen.gd
+++ scenes/splash-screen/SplashScreen.gd
@@ -1,13 +1,16 @@
 extends Control
 
 func _ready() -> void:
-	$AnimationPlayer.play("fade-in")
+	if GameAutoload.DEBUG:
+		load_main_scene()
+	else:
+		$AnimationPlayer.play("fade-in")
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_cancel"):
 		load_main_scene()
 
-func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+func _on_AnimationPlayer_animation_finished(_anim_name: String) -> void:
 	load_main_scene()
 
 func load_main_scene() -> void:
