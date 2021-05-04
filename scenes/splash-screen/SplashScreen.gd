594bf851d6ecdd3d5c1c620c738b110973d74b84
damien <damiengabrielle@gmail.com>
2021-05-04 19:30:25 +0200

    fade out to quit splash screen

--- scenes/splash-screen/SplashScreen.gd
+++ scenes/splash-screen/SplashScreen.gd
@@ -1,10 +1,16 @@
 extends Control
 
+onready var tween: Tween = $Tween
+onready var fade_out: ColorRect = $FadeOut
+var loaded = false
+
 func _ready() -> void:
+	loaded = false
+	fade_out.color = Color(0, 0, 0, 0)
 	if GameAutoload.DEBUG:
 		load_main_scene()
 	else:
-		$AnimationPlayer.play("fade-in")
+		$AnimationPlayer.play("intro")
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_cancel"):
@@ -14,4 +20,11 @@ func _on_AnimationPlayer_animation_finished(_anim_name: String) -> void:
 	load_main_scene()
 
 func load_main_scene() -> void:
+	if loaded:
+		return
+	loaded = true
+	tween.interpolate_property(fade_out, "color", fade_out.color, Color(0, 0, 0, 1), 0.4)
+	tween.start()
+
+func _on_Tween_tween_all_completed():
 	get_tree().change_scene("res://scenes/levels/map/HubLevel.tscn")
